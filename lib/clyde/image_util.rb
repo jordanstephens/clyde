f1dae772cd4f6fedf2d0d9ba72db9a57718e6f73
Jordan Stephens <iam@jordanstephens.net>
2013-12-28 22:18:39 -0500

    reverting back to trying md5 for image diffs first
    if images are not identical (hashes are not identical) then we can fall
    back to a detailed comparison procedure.
    
    i was initially under the impression that the detailed comparison loop
    took the most time. However, after running some timers, it's clear that
    the bottleneck is reading the images into ChunkyPNG, frequently taking
    close to a minute.

--- lib/clyde/image_util.rb
+++ lib/clyde/image_util.rb
@@ -3,16 +3,18 @@ require "chunky_png"
 module Clyde
   module ImageUtil
     def self.pixel_difference(path_a, path_b)
-      images = [path_a, path_b].map do |path|
-        ChunkyPNG::Image.from_file(path)
-      end
 
       # compare hashes first to see if the images are identical
       # if they are not identical, use a more detailed (and much
       # slower) diff method to obtain the degree of difference
-      return 0.0 if identical?(*images)
+      return 0.0 if identical?(path_a, path_b)
+
+      images = [path_a, path_b].map do |path|
+        ChunkyPNG::Image.from_file(path)
+      end
 
       different_pixel_count = 0
+
       images.first.height.times do |y|
         images.first.row(y).each_with_index do |pixel, x|
           different_pixel_count += 1 unless pixel == images.last[x, y]
@@ -22,8 +24,9 @@ module Clyde
       (different_pixel_count.to_f / images.first.pixels.length).round(3)
     end
 
-    def self.identical?(image_a, image_b)
-      image_a.hash == image_b.hash
+    def self.identical?(path_a, path_b)
+      [path_a, path_b].map { |path| Digest::MD5.file(path) }
+                      .uniq { |hash| hash.digest }.length == 1
     end
   end
 end
